467aee4db770937d9f16bd4ffd91a48f430cfeca
Victor Westerlund <victor.vesterlund@gmail.com>
2020-12-01 16:56:20 +0100

    2.0.0-dev12011451
    "spritesheet/generate.py" - Added automatic sprite sheet scaling to generator and removed fixed values.
    
    "public/index.html" - Added "Next" button to the last configurator block. Added temporary grid structure for pages. This should ofc be autogenerated
    
    Moved sprite sheets into their own folder "./assets/img/sheets"
    
    Replaced configurator "data" attribute with an inline style="--page:0" where the current page will update rel. CSS "automatically"

--- spritesheet/generate.py
+++ spritesheet/generate.py
@@ -1,28 +1,47 @@
 import json
+import math
 from pysheeter import PySheeter # https://github.com/VictorWesterlund/pysheeter/
 
 folder = (
 	"configurator/",
 	"thumbs/",
-	"../public/assets/img/" # Destination folder
+	"../public/assets/img/sheets/" # Destination folder
 )
 
-# Initialize spritesheets
+sizes = 3 # No. of scaled sheets
+cSheetRect = (872,581) # Configurator sheet dimensions
+tSheetSquare = 256 # Thumbail sheet dimensions (width and height)
+
+# Create new sheets
 configurator = PySheeter.Sheet()
 thumb = PySheeter.Sheet()
 
 with open("../public/config.json","r") as f:
 	data = json.load(f)
 
-# Load images into spritesheets
+# Load images into sheets
 for key in data["products"]:
 	configurator.add(f"{folder[0]}{key}.png")
 	thumb.add(f"{folder[1]}{key}.png")
 
 # -----------
 
-configurator.put(folder[2] + "sprites_c872.png",(872,581)) # Full-size
-configurator.put(folder[2] + "sprites_c436.png",(436,240)) # Mobile
+# Generate scaled sheets
+# Example: configurator.put("../public/assets/img/sheets/c_512x512.png",(512,512))
+for i in range(sizes):
+	if(i == 0):
+		# 1x (original) size
+		configurator.put(f"{folder[2]}c_{cSheetRect[0]}x{cSheetRect[1]}.png",cSheetRect)
+		thumb.put(f"{folder[2]}t_{tSheetSquare}x{tSheetSquare}.png",(tSheetSquare,tSheetSquare))
+		continue
+	
+	# Scale dimensions
+	factor = i * 1.5 # Limit factor
+	cSheetRectScaled = (
+		math.floor(cSheetRect[0] / factor),
+		math.floor(cSheetRect[1] / factor)
+	)
+	tSheetSquareScaled = math.floor(tSheetSquare / factor)
 
-thumb.put(folder[2] + "sprites_t256.png",(256,256)) # Full-size
-thumb.put(folder[2] + "sprites_t128.png",(128,128)) # Mobile
+	configurator.put(f"{folder[2]}c_{cSheetRectScaled[0]}x{cSheetRectScaled[1]}.png",cSheetRectScaled)
+	thumb.put(f"{folder[2]}t_{tSheetSquareScaled}x{tSheetSquareScaled}.png",(tSheetSquareScaled,tSheetSquareScaled))
